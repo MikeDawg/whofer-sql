ec09d652f10da494699c2d0af569a128f5656492
Mike Harris <MikeDawg@gmail.com>
2018-08-29 22:25:09 -0700

    More fixes

--- whofer.py
+++ whofer.py
@@ -39,6 +39,9 @@ import argparse
 import array
 import re
 import sqlite3
+import random
+import uuid
+import errno
 from datetime import datetime
 
 #----
@@ -48,8 +51,18 @@ parser.add_argument('-d','--domain', help='Domain Name',required=True)
 args = parser.parse_args()
 
 #---- Variables
-datestr = "{0.year}-{0.month}-{0.day}".format(datetime.now())
-db_name = ("%s" % datestr)
+datestr = "{0.year}-{00.month}-{0.day}".format(datetime.now())
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4())
+    random = random.upper()
+    random = random.replace("-","")
+    return random[0:string_length]
+#----
+random_string_name = my_random_string(6)
+random_date_str = datestr + random_string_name
+db_name = ("%s" % random_date_str)
 domain_name_from_user = (args.domain)
 current_working_directory = os.getcwd()
 w = whois.whois(domain_name_from_user)
@@ -59,16 +72,34 @@ emails_from_whois = (w.emails)
 # -- Create db
 print db_name
 
-fileno, tempfile_output = tempfile.mkstemp(suffix='.tmp',prefix='.whofer_email_',text=True,dir=current_working_directory)
-print(tempfile_output)
-try:
-    fd = open(tempfile_output, mode='w')
-    for item in emails_from_whois:
-        fd.write("%s" % domain_name_from_user)
-        fd.write("\t")
-        fd.write("%s\n" % item)
-finally:
-    fd.close()
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: 
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 #----
+filename = "./data"
+if (mkdir_p(filename)):
+        print "Created dir : %s" % (os.path.dirname(filename))
+
+db = sqlite3.connect('data/whofer.sqlite3')
+cursor = db.cursor()
+cursor.execute('''
+        Create TABLE IF NOT EXISTS whofer(id INTEGER PRIMARY KEY, URL TEXT, addy TEXT)
+        ''')
+db.commit()
+    #----
+for item in emails_from_whois:
+    cursor.execute('''INSERT INTO whofer(URL, addy)
+    VALUES(?,?)''', (domain_name_from_user,item))
+    db.commit()
+#----
+db = sqlite3.connect('data/whofer.sqlite3')
+cursor.execute('''SELECT URL, addy FROM whofer''')
+for row in cursor:
+    print('{0} : {1}'.format(row[0], row[1]))
     
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
